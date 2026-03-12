f647c7f280d8679b14d33a4020b85af035e126a2
Matheus Christo <matheuschristo.09@gmail.com>
2026-03-12 19:53:22 -0300

    Initial commit

---
+++ src/main/java/br/com/api/mace/replicacao/MaceReplicacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.mace.replicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaceReplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaceReplicacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/mace/replicacao/ApiMaceReplicacaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.mace.replicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMaceReplicacaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
